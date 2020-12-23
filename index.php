4689c52b353e35f44f0efc375d44802b170a192b
tushara05 <70750978+tushara05@users.noreply.github.com>
2020-12-23 10:42:27 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>E-MEDICAL EMERGENCY SERVICES</title>
+    <link rel="stylesheet" type="text/css" href="style1.css">
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="footer, address, phone, icons" />
+
+
+
+	<link rel="stylesheet" href="style1.css">
+	
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+
+	<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet">
+</head>
+<body>
+    <!--start header-->
+    <header>
+        <h1 class="title">E-MEDICAL EMERGENCY SERVICES</h1>
+
+        <div >
+            <ul>
+                <li><a href="book.php" class="button">Book</a></li>
+                <li><a href="track.php" class="button">Track</a></li>
+            </ul>
+        </div>
+       
+
+        
+    </header>
+
+    <!--!end header--> 
+    <!--start footer-->
+    
+        <footer class="footer-distributed">
+
+			<div class="footer-left">
+          
+				<h3>E-MES</h3>
+
+				<p class="footer-links">
+					<a href="#">About</a>
+					|
+					<a href="#">Contact</a>
+				</p>
+
+				<p class="footer-company-name">E-MEDICAL EMERGENCY SERVICES</p>
+			</div>
+
+			<div class="footer-center">
+				<div>
+					<i class="fa fa-map-marker"></i>
+					  <p><span>Akshay Nagar 1st Block 1st Cross</span>
+						Rammurthy nagar,Bangalore-560016</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+91 22-27782183</p>
+				</div>
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:Emescontact@gmail.com">Emescontact@gmail .com</a></p>
+				</div>
+			</div>
+			<div class="footer-right">
+				<p class="footer-company-about">
+					<span>About Us</span>
+					We are here to save the lives of people by offering them immediate traumacare, by providing the availability of ambulance.
+Also one can book an appointment with doctors.</p>
+				<div class="footer-icons">
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					
+					<a href="#"><i class="fa fa-linkedin"></i></a>
+					
+				</div>
+			</div>
+		</footer>
+       
+
+   
+    <!--!end footer-->
+</body>
+</html>
