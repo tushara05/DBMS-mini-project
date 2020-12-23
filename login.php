59430d57390e4f620bf89c180521483b72208b84
tushara05 <70750978+tushara05@users.noreply.github.com>
2020-12-23 08:55:28 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+     <title>User Login and Registration</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+    <div class="login-box">
+    <div class="row">
+    <div class="col-md-6 login-left">
+         <h2> Login here </h2>
+         <form action="validation.php" method="post">
+             <div class="form-group">
+                 <label>Username</label>
+                 <input type="text" name="user" class="form-control" required>
+                 </div>
+            <div class="form-group">
+                 <label>Password</label>
+                 <input type="password" name="password" class="form-control" required>
+                 </div>
+                 <button type="submit" class="btn btn-primary">Login</button>       
+        </form>
+    </div>
+    <div class="col-md-6 login-right">
+         <h2> Register here </h2>
+         <form action="Registration.php" method="post">
+             <div class="form-group">
+                 <label>Username</label>
+                 <input type="text" name="user" class="form-control" required>
+                 </div>
+            <div class="form-group">
+                 <label>Password</label>
+                 <input type="password" name="password" class="form-control" required>
+                 </div>
+                 <button type="submit" class="btn btn-primary">Register</button>       
+
+        </form>
+    </div>
+
+    </div>
+    
+
+
+
+    </div>
+
+</div>
+</body>
+</html>      
